441e9dd885e3291c4dc1c8fe5b69cbeca7637208
KostIlya <ilya.kostylev.1997@mail.ru>
2026-02-10 09:54:18 +0300

    fix 11

--- src/main/java/ru/bell/auto_form/config/WebDriverFactory.java
+++ src/main/java/ru/bell/auto_form/config/WebDriverFactory.java
@@ -13,7 +13,10 @@ public class WebDriverFactory {
     public static WebDriver createDriver() {
         try {
 //            WebDriverManager.chromedriver().setup();
-            System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+            String os = System.getProperty("os.name").toLowerCase();
+            if (os.contains("lin")) {
+                System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+            }
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
             options.addArguments("--window-size=1920,1080");
@@ -22,7 +25,9 @@ public class WebDriverFactory {
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-blink-features=AutomationControlled");
             options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
-            options.setBinary("/usr/local/bin/google-chrome");
+            if (os.contains("lin")) {
+                options.setBinary("/usr/local/bin/google-chrome");
+            }
             return new ChromeDriver(options);
         } catch (Exception e) {
             log.error("Error created WebDriver", e);
